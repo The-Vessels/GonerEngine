b9ae980612d098b09a64c1c8b539da3f0675f643
KrisGra <39800337+krisgrant@users.noreply.github.com>
2025-09-01 00:26:16 +0300

    BOOM COMMIT BOOM COMMIT here you go

--- Scripts/textbox.gd
+++ Scripts/textbox.gd
@@ -1,16 +1,19 @@
 extends Control
 
-var text = "* Yo Kris... I'm like, test dialogue and stuff..."
+var text = "* Dreemurr residence, is
+	this real? uh, uh uh,
+	uh uh, hu hu huh uh uhu"
 @onready var dialoguetext: Label = $Box/dialoguetext
 @onready var talkblip: AudioStreamPlayer = $talkblip
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	dialoguetext.text = ""
 	for char in text:
 		dialoguetext.text += char
 		if not (char == "*" or char == " "):
 			talkblip.play()
-		await get_tree().create_timer(0.001).timeout
+		await get_tree().physics_frame
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
