f3e7e2fc87c39c84a646f5470331d5d59fa36a05
KrisGra <39800337+krisgrant@users.noreply.github.com>
2025-09-04 06:02:09 +0300

    update textbox script
    it don't jump from one line to another anymore

--- Scripts/textbox.gd
+++ Scripts/textbox.gd
@@ -1,19 +1,26 @@
 extends Control
 
-var text = "* Dreemurr residence, is
-	this real? uh, uh uh,
-	uh uh, hu hu huh uh uhu"
+var text = "* Dreemurr residence, whooooo is this?"
 @onready var dialoguetext: Label = $Box/dialoguetext
 @onready var talkblip: AudioStreamPlayer = $talkblip
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	dialoguetext.text = ""
+	dialoguetext.visible_characters_behavior = TextServer.VC_CHARS_AFTER_SHAPING
+	dialoguetext.visible_characters = 0
+	dialoguetext.text = text
+	if text[0] == "*":
+		dialoguetext.visible_characters = 1
 	for char in text:
-		dialoguetext.text += char
-		if not (char == "*" or char == " "):
-			talkblip.play()
+		dialoguetext.visible_characters += 1
+		talkblip.play()
 		await get_tree().physics_frame
+	#dialoguetext.text = ""
+	#for char in text:
+		#dialoguetext.text += char
+		#if not (char == "*" or char == " "):
+			#talkblip.play()
+		#await get_tree().physics_frame
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
