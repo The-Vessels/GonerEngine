57ad8df0a6bbddc34cf97bdf99a4666e2b45a3d8
Anon Ymous <ancientstraits@protonmail.com>
2025-09-28 15:38:51 -0400

    Added fun gang walk/face sprites

---
+++ scripts/common/circular_queue.gd
@@ -0,0 +1,28 @@
+class_name CircularQueue
+
+var positions: PackedVector2Array
+var max_length: int
+var idx: int = 0
+var reset: bool = true
+
+func _init(max_length: int) -> void:
+	self.max_length = max_length
+	positions = PackedVector2Array()
+	positions.resize(max_length)
+
+func add(value: Vector2) -> void:
+	if reset:
+		reset = false
+		positions.fill(value)
+	else:
+		positions[idx] = value
+	idx += 1
+	if idx >= max_length:
+		idx = 0
+
+# get(0) returns the most recently added position
+# get(1) returns the 2nd most recently added position
+# get(49) returns the 50th most recently added position
+# Try not to get() an index larger than max_length
+func get_pos(index: int) -> Vector2:
+	return positions[posmod(-index + idx, max_length)]
