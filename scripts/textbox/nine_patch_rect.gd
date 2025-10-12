1000f5e08345e92a810064ee360f245150dd8786
Anon Ymous <ancientstraits@protonmail.com>
2025-10-12 02:03:02 -0400

    Fixed problems with case sensitivity :D

--- scripts/textbox/nine_patch_rect.gd
+++ scripts/textbox/nine_patch_rect.gd
@@ -3,6 +3,6 @@ extends NinePatchRect
 func _ready():
 	match Global.world_type:
 		Global.WORLD_LIGHT:
-			texture = load("res://Assets/Sprites/textbox/light/lighttextbox.png")
+			texture = load("res://assets/sprites/textbox/light/lighttextbox.png")
 		Global.WORLD_DARK:
-			texture = load("res://Assets/Sprites/textbox/dark/darktextbox1.png")
+			texture = load("res://assets/sprites/textbox/dark/darktextbox1.png")
