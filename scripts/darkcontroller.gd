ef42fc8cc8121dcf72ec037ac904fb2bc1e62693
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-03-03 02:23:57 +0200

    darkcontroller top btn testing

--- scripts/darkcontroller.gd
+++ scripts/darkcontroller.gd
@@ -1,10 +1,34 @@
 extends Control
+@onready var darkcontroller: Control = $"."
+@onready var dark_menu_btn_container: HBoxContainer = $TopRect/DarkMenuBtnContainer
+@onready var dark_item_btn: TextureButton = $TopRect/DarkMenuBtnContainer/DarkItemBtn
+@onready var dark_item_menu: Control = $TopRect/DarkMenuBtnContainer/DarkItemBtn/DarkItemMenu
+@onready var top_rect: ColorRect = $TopRect
+@onready var bottom_rect: ColorRect = $BottomRect
+const CONFIG_H = preload("uid://c7xa7pmx162nx")
 
-@onready var dark_item_btn: TextureButton = $DarkMenuBtnContainer/DarkItemBtn
-@onready var dark_menu_btn_container: HBoxContainer = $DarkMenuBtnContainer
+var menuOpen := false
 
 func _ready():
-	dark_item_btn.grab_focus.call_deferred()
+	dark_menu_btn_container.get_child(0).grab_focus.call_deferred()
 	for child: TextureButton in dark_menu_btn_container.get_children():
-		child.focus_exited.connect(func(): Global.play_ui_sound('menumove'))
-		child.pressed.connect(func(): Global.play_ui_sound('select'))
+		child.focus_exited.connect(
+			func():
+				Global.play_ui_sound('menumove')
+		)
+		child.pressed.connect(
+			func():
+				Global.play_ui_sound('select')
+				child.get_child(0).visible = true
+				child.get_child(0).get_child(0).grab_focus.call_deferred()
+		)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("menu", false) && menuOpen == false:
+		top_rect.position.y += top_rect.size.y
+		bottom_rect.position.y -= bottom_rect.size.y
+		menuOpen = true
+	elif event.is_action_pressed("menu", false) && menuOpen == true:
+		top_rect.position.y -= top_rect.size.y
+		bottom_rect.position.y += bottom_rect.size.y
+		menuOpen = false
