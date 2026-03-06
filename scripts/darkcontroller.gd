cf5750a5e4696485cd21b9b4847c74bfa9366843
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-03-06 18:15:11 +0200

    Add DarkBox Scene and fix DarkBox Sprites

--- scripts/darkcontroller.gd
+++ scripts/darkcontroller.gd
@@ -6,6 +6,7 @@ extends Control
 @onready var top_rect: ColorRect = $TopRect
 @onready var bottom_rect: ColorRect = $BottomRect
 @onready var dark_menu_desc: Sprite2D = $TopRect/DarkMenuDescContainer/DarkMenuDesc
+@onready var dark_submenu_container: Control = $DarkSubmenuContainer
 
 const ITEM = preload("uid://crc76dli0yory")
 const EQUIP = preload("uid://daa65pt526k5b")
@@ -21,7 +22,8 @@ var DarkBtnImgs = [
 
 func _ready():
 	dark_menu_btn_container.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_DISABLED
-	
+	dark_submenu_container.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_DISABLED
+
 func _process(delta: float) -> void:
 	for child in bottom_rect.get_children(false):
 		if child.get_index() == Global.currentHero:
@@ -31,22 +33,22 @@ func _process(delta: float) -> void:
 	dark_menu_desc.texture = DarkBtnImgs[Global.selectedDarkBtn]
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("menu", false) && Global.darkMenuOpened == false:
+	if event.is_action_pressed("menu", false) && !Global.darkMenuOpened:
 		top_rect.position.y += top_rect.size.y
 		bottom_rect.position.y -= bottom_rect.size.y + 2 # +2 to account for charbox top line
 		dark_menu_btn_container.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_ENABLED
 		dark_menu_btn_container.get_child(Global.selectedDarkBtn).grab_focus()
 		Global.darkMenuOpened = true
-	elif (event.is_action_pressed("menu", false) || event.is_action_pressed("cancel", false)) && Global.darkMenuOpened == true:
+	elif (event.is_action_pressed("menu", false) || event.is_action_pressed("cancel", false)) && Global.darkMenuOpened && !Global.darkSubmenuOpened:
 		top_rect.position.y -= top_rect.size.y
 		bottom_rect.position.y += bottom_rect.size.y + 2 # +2 to account for charbox top line
 		dark_menu_btn_container.focus_behavior_recursive = Control.FOCUS_BEHAVIOR_DISABLED
 		Global.darkMenuOpened = false
-	elif event.is_action_pressed("right") && Global.darkMenuOpened == true:
+	elif event.is_action_pressed("right") && Global.darkMenuOpened:
 		Global.currentHero += 1
 		if Global.currentHero >= bottom_rect.get_child_count(false):
 			Global.currentHero = 0
-	elif event.is_action_pressed("left") && Global.darkMenuOpened == true:
+	elif event.is_action_pressed("left") && Global.darkMenuOpened:
 		Global.currentHero -= 1
 		if Global.currentHero < 0:
 			Global.currentHero = bottom_rect.get_child_count(false) - 1
