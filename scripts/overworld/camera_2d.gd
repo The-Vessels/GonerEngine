8ac2c294152cf22b30b0f082e39dd1fd6c9aecaa
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-05-03 21:09:29 +0300

    Not very good camera stuff but camera stuff

---
+++ scripts/overworld/camera_2d.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+@onready var tile_map_layer: TileMapLayer = $"../../TileMapLayer"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tilemap_size = tile_map_layer.get_used_rect()
+	var tilemap_tile_size = tile_map_layer.tile_set.tile_size
+	
+	# Calculate pixel boundaries
+	var world_coords = tilemap_size.position * tilemap_tile_size * 2
+	var end_coords = tilemap_size.end * tilemap_tile_size * 2
+	
+	print(world_coords)
+	print(end_coords)
+	
+	limit_left = int(world_coords.x)
+	limit_top = int(world_coords.y)
+	limit_right = int(end_coords.x)
+	limit_bottom = int(end_coords.y)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/overworld/teleport_area.gd
+++ scripts/overworld/teleport_area.gd
@@ -1,6 +1,7 @@
 class_name TeleportArea extends Area2D
 
-@export_file("*.tscn") var destination_scene
+@export_file("*.tscn") var target_scene
+@export var target_marker_id: int
 
 @onready var scene_container: Node = get_tree().root \
 	.get_child(-1).get_node("SceneContainer")
@@ -9,11 +10,11 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 	print('HIIIIII ', scene_container)
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	var child := scene_container.get_child(0)
 	if child != null:
 		child.queue_free()
 	if scene_container.get_child_count() == 1:
-		var scene: PackedScene = load(destination_scene)
+		var scene: PackedScene = load(target_scene)
 		var instantiated := scene.instantiate()
 		scene_container.add_child.call_deferred(instantiated)
