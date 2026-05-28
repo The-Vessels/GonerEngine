6363774e130382b036d03fc601d1ccbfc4903289
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-05-29 00:07:53 +0300

    Remove unused scripts finally

--- scripts/textbox/talk_sprite.gd
+++
@@ -1,8 +0,0 @@
-extends TextureRect
-
-func _ready():
-	match Global.world_type:
-		Global.WORLD_LIGHT:
-			set_position(Vector2(64.0, 350.0))
-		Global.WORLD_DARK:
-			set_position(Vector2(69.0, 350.0))
