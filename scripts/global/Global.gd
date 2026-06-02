7396edd28b7fc14f15a2a2c8e59b35f947f421e7
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-06-03 00:26:29 +0300

    play_ui_sound file path fix

--- scripts/global/Global.gd
+++ scripts/global/Global.gd
@@ -76,7 +76,7 @@ func setup_discord_rpc():
 	DiscordRPC.refresh()
 
 func play_ui_sound(sound_name: String):
-	$UIAudioPlayer.stream = load('res://assets/sounds/ui/' + sound_name + '.wav')
+	$UIAudioPlayer.stream = load('res://sounds/ui/' + sound_name + '.wav')
 	$UIAudioPlayer.play()
 
 # Used for a room's border node to set the dynamic border.
