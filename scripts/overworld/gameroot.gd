05947ef2666f6c56f203a114389c96bd61150038
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-07-13 19:24:50 +0300

    Move menu logic to MenuLayer

--- scripts/overworld/gameroot.gd
+++ scripts/overworld/gameroot.gd
@@ -1,21 +1,7 @@
 # This is the root node of the main game.
-# It handles: game scaling and switching the menu based on starting world type
+# For now it just handles scaling the game
 
 extends Node2D
-
-@onready var menu_layer: CanvasLayer = $MenuLayer
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var menu_scene: PackedScene
-	if Global.is_dark():
-		menu_scene = load("res://scenes/ui/dark_menu.tscn")
-	else:
-		menu_scene = load("res://scenes/ui/light_menu.tscn")
-	
-	var menu_node: Control = menu_scene.instantiate()
-	menu_node.visible = false
-	menu_layer.add_child(menu_node)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
