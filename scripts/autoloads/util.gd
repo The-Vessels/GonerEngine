298bdcedd2b09d62814510bd7b43fad0d4cedfee
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-08-19 01:31:39 +0300

    Create realistic_explosion scene

--- scripts/autoloads/util.gd
+++ scripts/autoloads/util.gd
@@ -28,11 +28,12 @@ func frames_to_sec(frames: float) -> float:
 	return frames / 30.0
 
 ## Explode a Node2D
-func explode(node: Node2D, scale: Variant = Vector2(1.0, 1.0), speed: float = 1.0, offset: Vector2 = Vector2.ZERO) -> void:
+func explode(node: Node2D, scale: Variant = Vector2(1.0, 1.0), speed: float = 1.0, volume_scale: float = 1.0, offset: Vector2 = Vector2.ZERO) -> void:
 	var explode_node = AnimatedSprite2D.new()
 	explode_node.name = "RealisticExplosion"
 	explode_node.sprite_frames = load("res://sprites/misc/realistic_explosion/realistic_explosion.tres")
 	explode_node.speed_scale = speed
+	explode_node.y_sort_enabled = true
 	
 	if scale is Vector2 or scale is float or scale is int:
 		if !(scale is Vector2):
@@ -51,7 +52,7 @@ func explode(node: Node2D, scale: Variant = Vector2(1.0, 1.0), speed: float = 1.
 	
 	node.queue_free()
 	explode_node.play()
-	Global.play_sound("misc/snd_badexplosion")
+	Global.play_sound("misc/snd_badexplosion", volume_scale)
 	
 	await explode_node.animation_finished
 	explode_node.queue_free()
